0dadb9b70495a6c45435da858a8fad477a4bcc5d
wiinuk <wii.nukegara@gmail.com>
2020-12-09 19:22:51 +0900

    🚧

---
+++ server/test.fsx
@@ -0,0 +1,13 @@
+open System
+open System.IO
+
+let localPath = "file.ext"
+printfn $"localPath: {localPath}"
+let currentDir = Environment.CurrentDirectory
+printfn $"currentDir: {currentDir}"
+let absolutePath  = Path.Combine(currentDir, path)
+printfn $"absolutePath: {absolutePath}"
+let absoluteUri = Uri absolutePath
+printfn $"absoluteUri: {absoluteUri}"
+let absoluteUriToPath = Uri("file:///" + absoluteUri.LocalPath).LocalPath
+printfn $"absoluteUriToPath: {absoluteUriToPath}"
