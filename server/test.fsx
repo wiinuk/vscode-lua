90a1557d71a7d9d71752dfa78538745de18a0abb
wiinuk <wii.nukegara@gmail.com>
2020-12-09 19:39:14 +0900

    更新

--- server/test.fsx
+++ server/test.fsx
@@ -2,14 +2,14 @@ open System
 open System.IO
 
 let localPath = "file.ext"
-printfn $"localPath: {localPath}"
+printfn "localPath: %s" localPath
 let currentDir = Environment.CurrentDirectory
-printfn $"currentDir: {currentDir}"
+printfn "currentDir: %s" currentDir
 let absolutePath  = Path.Combine(currentDir, localPath)
-printfn $"absolutePath: {absolutePath}"
+printfn "absolutePath: %s" absolutePath
 let absoluteUri = Uri absolutePath
-printfn $"absoluteUri: {absoluteUri}"
+printfn "absoluteUri: %A" absoluteUri
 let absoluteUriToLocalPath = absoluteUri.LocalPath
-printfn $"absoluteUriToLocalPath: {absoluteUriToLocalPath}"
-let absoluteUriToPath = Uri("file:///" + absoluteUriToLocalPath).LocalPath
-printfn $"absoluteUriToPath: {absoluteUriToPath}"
+printfn "absoluteUriToLocalPath: %s" absoluteUriToLocalPath
+let absoluteUriToPath = Uri("file://" + absoluteUriToLocalPath).LocalPath
+printfn "absoluteUriToPath: %s" absoluteUriToPath
