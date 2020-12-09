768d995ad4bc6d9b08c190150e8699d91a4859ec
wiinuk <wii.nukegara@gmail.com>
2020-12-09 19:26:37 +0900

    修正

--- server/test.fsx
+++ server/test.fsx
@@ -5,7 +5,7 @@ let localPath = "file.ext"
 printfn $"localPath: {localPath}"
 let currentDir = Environment.CurrentDirectory
 printfn $"currentDir: {currentDir}"
-let absolutePath  = Path.Combine(currentDir, path)
+let absolutePath  = Path.Combine(currentDir, localPath)
 printfn $"absolutePath: {absolutePath}"
 let absoluteUri = Uri absolutePath
 printfn $"absoluteUri: {absoluteUri}"
