c9c312ffeba1565949d925a331c588e496b377ad
wiinuk <wii.nukegara@gmail.com>
2020-12-12 11:28:46 +0900

    sendResponse を追加

--- server/src/server/WriteAgent.fs
+++ server/src/server/WriteAgent.fs
@@ -2,16 +2,27 @@
 module LuaChecker.Server.WriteAgent
 open LuaChecker.Server.Log
 open LuaChecker.Server.Protocol
+open System
 open System.Text
 open type WriteAgent
 
+
+let sendResponse (agent: _ MailboxProcessor) id result =
+    let jsonBytes =
+        match result with
+        | Ok result -> JsonRpcMessage.successResponse id result
+        | Error e -> JsonRpcMessage.errorResponse id e
+        |> Json.serialize
+        |> ReadOnlyMemory
+
+    agent.Post <| WriteMessage jsonBytes
 
 let create agent = new MailboxProcessor<_>(fun inbox ->
     let rec loop agent = async {
         match! inbox.Receive() with
         | QuitWriteAgent -> return ()
         | WriteMessage message ->
-            ifDebug { Log.Format(agent.resources.LogMessages.RequestSending, Encoding.UTF8.GetString message.Span) }
+            ifDebug { Log.Format(agent.resources.LogMessages.MessageSending, Encoding.UTF8.GetString message.Span) }
             MessageWriter.writeRawMessage agent.writer message.Span
 
             return! loop agent
