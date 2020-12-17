69391e549b0047ec8e3fe8c6d6a6e11e3ac7b06d
wiinuk <wii.nukegara@gmail.com>
2020-12-17 19:04:08 +0900

    修正

--- server/test/Checker.Tests/Tests.fs
+++ server/test/Checker.Tests/Tests.fs
@@ -6,12 +6,7 @@ open Xunit
 
 [<Fact>]
 let escapedPath() =
-    let localPath =
-        Uri "file:///c%3A/dir/file.ext"
-        |> DocumentPath.ofUri
-        |> DocumentPath.toPathOrFail
-
-    if Environment.OSVersion.Platform = PlatformID.Win32NT then
-        localPath =? "c:\\dir\\file.ext"
-    else
-        localPath =? "/c:/dir/file.ext"
+    Uri "file:///c%3A/dir/file.ext"
+    |> DocumentPath.ofUri
+    |> DocumentPath.toPathOrFail
+    =? "c:\\dir\\file.ext"
